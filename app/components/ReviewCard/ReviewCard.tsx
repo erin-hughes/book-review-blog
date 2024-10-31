f409d6e3eb3d1c4beee8a1997732afc3878ed93b
Erin Hughes <erin.hughes@ibm.com>
2024-10-31 10:45:58 +0000

    fix: test hardcoding image path

--- app/components/ReviewCard/ReviewCard.tsx
+++ app/components/ReviewCard/ReviewCard.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { ReviewCardProps } from "./interfaces/ReviewCardProps";
 
@@ -7,18 +7,18 @@ const ReviewCard = ({ title, slug, imageSrc }: ReviewCardProps) => {
     <Link key={slug} href={`/reviews/${slug}`}>
       <div className="bg-stone-800 p-4 flex flex-col items-center group">
         <div className="w-full h-[400px] overflow-hidden rounded">
-          {/* <img
-            src={imageSrc}
+          <img
+            src={`/book-review-blog${imageSrc}`}
             alt={title}
             className="object-cover w-full h-full grayscale opacity-50 group-hover:grayscale-0 group-hover:opacity-100 transition duration-200 ease-in-out"
-          /> */}
-          <Image
+          />
+          {/* <Image
             src={imageSrc}
             alt={title}
             className="object-cover w-full h-full grayscale opacity-50 group-hover:grayscale-0 group-hover:opacity-100 transition duration-200 ease-in-out"
             width={0}
             height={0}
-          />
+          /> */}
         </div>
         <h3 className="mt-4 text-center text-lg font-ebGaramond">{title}</h3>
       </div>
